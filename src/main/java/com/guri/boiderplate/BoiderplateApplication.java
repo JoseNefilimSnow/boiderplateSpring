dd0837cda52250d522edc220e9ac601ec76f0a23
Jose J Diaz Vega <josejdiazvega@gmail.com>
2022-10-22 00:12:06 +0100

    Initial commit

---
+++ src/main/java/com/guri/boiderplate/BoiderplateApplication.java
@@ -0,0 +1,13 @@
+package com.guri.boiderplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoiderplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoiderplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guri/boiderplate/BoiderplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guri.boiderplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoiderplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
